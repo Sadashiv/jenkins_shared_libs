034e823af8bb3b03d7809a6ada46f5becc6a5b73
Sadashiv H <sadamsrit@gmail.com>
2022-01-20 16:31:08 +0530

    Added print for this

--- src/com/sadashiv/jenkins/pipeline/initiate/General.groovy
+++ src/com/sadashiv/jenkins/pipeline/initiate/General.groovy
@@ -2,6 +2,8 @@ import groovy.transform.Field
 import hudson.model.Cause
 
 
+@Field boolean allowManualBuilds
+
 timestamps {
 
     ansiColor('xterm') {
@@ -9,9 +11,12 @@ timestamps {
         node('agent1') {
             stage('Initiate') {
                 echo "Started running"
+                if (!allowManualBuilds && currentBuild.rawBuild.getCause(Cause.UserIDCause)) {
+                    errot("Can't run this jenkins job manually")
+                }
             }
             String flowName = "com.sadashiv.jenkins.pipeline.CI"
-            class flowClass = Class.forName(flowName, true)//, this.class.classLoader)
+      //     class flowClass = Class.forName(flowName, true)//, this.class.classLoader)
       //      flow = flowClass.getrConstructor(object.class) newInstance(this)
             echo "Flow ${flowName}"
 
