e127e4499218c8c5fdc175f1859ff80afc231c9f
Sadashiv H <sadamsrit@gmail.com>
2022-01-20 16:39:25 +0530

    Added print for this

--- src/com/sadashiv/jenkins/pipeline/General.groovy
+++ src/com/sadashiv/jenkins/pipeline/General.groovy
@@ -16,7 +16,7 @@ timestamps {
                 }*/
             }
             String flowName = "com.sadashiv.jenkins.pipeline.CI"
-           class flowClass = Class.forName(flowName, true, this.class.classLoader)
+      //     class flowClass = Class.forName(flowName, true, this.class.classLoader)
       //      flow = flowClass.getrConstructor(object.class) newInstance(this)
             echo "Flow ${flowName}"
 
